71acb6506ec97092881c79871bf3d0a3e17d4644
Pallabi Chakraborty <pallavichakraborty@yahoo.com>
2019-02-14 10:45:18 -0600

    Completed Home Page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Featured from './featured/index';
 import Matches from './matches';
+import MeetPlayers from '../home/meetPlayers';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
        <div className="bck_blue">
          <Featured />
          <Matches/>
+         <MeetPlayers/>
        </div>
     );
 };
